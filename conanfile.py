456a4cff7f88833bf6cdd51244af71a17ef330b8
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:50 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,9 @@ from conans import ConanFile, tools
 class BoostVmdConan(ConanFile):
     name = "Boost.Vmd"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-vmd"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Preprocessor/1.65.1@bincrafters/testing"
 
     lib_short_names = ["vmd"]
@@ -16,21 +14,23 @@ class BoostVmdConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-vmd"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
 
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
         return super(self.__class__, self).env
+
+    def package_id(self):
+        self.info.header_only()
 
     # END
