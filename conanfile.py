51df6dfe22466bbde8dd3565e1e76802c69de4e7
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:37:57 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostVmdConan(base.BoostBaseConan):
     name = "boost_vmd"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_vmd"
     lib_short_names = ["vmd"]
     header_only_libs = ["vmd"]
